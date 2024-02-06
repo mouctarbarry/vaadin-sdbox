5397aea82807a8f8946d9941fdc0def8829eeacd
mouctarbarry <mouctar.barry@sogeti.com>
2024-02-06 13:30:19 +0100

    implementing CRM service

--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -1,6 +1,7 @@
 package com.example.application.views.list;
 
 import com.example.application.data.Contact;
+import com.example.application.services.CrmService;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
@@ -11,37 +12,36 @@ import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
-import java.util.Collections;
-
 @PageTitle("Contacts | Vaadin CRM")
 @Route(value = "")
 public class ListView extends VerticalLayout {
     Grid<Contact> grid = new Grid<>(Contact.class);
     TextField filterText = new TextField();
     ContactForm form;
-    
-    public ListView() {
+    CrmService service;    
+    public ListView(CrmService service) {
+        this.service = service;
         addClassName("list-view");
         setSizeFull();
         configureGrid();
         configureForm();
 
         add(getToolbar(), getContent());
+        updateList();
     }
 
     private Component getContent(){
         HorizontalLayout content = new HorizontalLayout(grid, form);
-        content.setFlexGrow(2, grid);
+        content.setFlexGrow(1, grid);
         content.setFlexGrow(1, form);
         content.addClassNames("content");
         content.setSizeFull();
         return content;
     }
     private void configureForm() {
-        form = new ContactForm(Collections.emptyList(), Collections.emptyList());
+        form = new ContactForm(service.findAllCompanies(), service.findAllStatuses());
         form.setWidth("25em");
     }
-
 
     private void configureGrid() {
         grid.addClassName("contact-grid");
@@ -56,12 +56,17 @@ public class ListView extends VerticalLayout {
         filterText.setPlaceholder("Filter by name...");
         filterText.setClearButtonVisible(true);
         filterText.setValueChangeMode(ValueChangeMode.LAZY);
+        filterText.addValueChangeListener(e -> updateList());
 
         Button addContact = new Button("Add contact");
-        addContact.getElement().setAttribute("class", "btn btn-primary");        
+       // addContact.getElement().setAttribute("class", "btn btn-primary");        
         HorizontalLayout toolbar = new HorizontalLayout(filterText, addContact);
         toolbar.addClassName("toolbar");
         return toolbar;
+    }
+
+    private void updateList() {
+        grid.setItems(service.findAllContacts(filterText.getValue()));
     }
 
 }
